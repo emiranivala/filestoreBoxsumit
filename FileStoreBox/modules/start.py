f06ab54fef5704a74a0b3d2ae16e2e99c2fb0f4a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-18 23:41:28 +0530

    Create start.py

---
+++ FileStoreBox/modules/start.py
@@ -0,0 +1,32 @@
+from pyrogram import filters
+from FileStoreBox import app
+from FileStoreBox.core import script
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+
+button = InlineKeyboardMarkup([[
+          InlineKeyboardButton("Tool Box", callback_data="tools_")                
+       ]])
+
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(_, message):        
+    if message.text.startswith("/start FileBox"):
+        pass
+        return 
+        
+    elif message.text.startswith("/start BatchBox"):
+        reffer_id = message.text.split("_")[1]
+        pass
+        return 
+    else:
+        await message.reply_photo(
+            photo="https://envs.sh/Hpz.jpg",
+            caption=script.START_TXT.format(message.from_user.first_name),
+            reply_markup=button
+        )
+
+  
