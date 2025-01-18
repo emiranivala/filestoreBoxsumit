db2b7c0f32b780a5b722998ea42e0f8ba244056c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-19 02:02:49 +0530

    Update script.py

--- FileStoreBox/core/script.py
+++ FileStoreBox/core/script.py
@@ -1,1 +1,26 @@
 START_TEXT = "Hi {}! \n\nI’m File Store Box, the perfect place to store your files and videos. With unlimited storage, I’m not just a bot—I’m your next-level storage partner!"
+
+TOOLS_TEXT = """
+**I am a File Store Box Bot with an unlimited database where users can store their media files securely.**
+
+**Key Features:**
+
+1. Batch Command (/batch):
+This command allows you to save multiple files at once.
+
+2. Media Links:
+Whenever you send or forward a media file, the bot automatically creates a unique link for it. You can use this link to access your media at any time.
+
+**Tools/Options:**
+
+**Database (Mandatory):**
+Adding your files to the database is required to generate links.
+
+**Shortener (Optional):**
+You can choose to shorten the generated links.
+
+**Force Channel (Optional):**
+Use this feature if necessary for specific actions or settings.
+
+**This bot simplifies file management and ensures your media is always accessible through shareable links.**
+"""
