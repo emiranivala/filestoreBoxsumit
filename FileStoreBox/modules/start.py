7c138d281899ba7a8c9640684bc3f26c2ec37b1e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-19 01:45:55 +0530

    Update start.py

--- FileStoreBox/modules/start.py
+++ FileStoreBox/modules/start.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from FileStoreBox import app
 from FileStoreBox.core import script
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 
@@ -30,3 +30,59 @@ async def start(_, message):
         )
 
   
+
+buttons = InlineKeyboardMarkup([
+      [
+          InlineKeyboardButton("Database", callback_data="database_"),          ,
+          InlineKeyboardButton("Force", callback_data="force_"),
+          InlineKeyboardButton("Shortener", callback_data="shortener_")
+      ],[   
+          InlineKeyboardButton("🔙 Back", callback_data="home_") 
+      ]])
+
+
+
+
+
+@app.on_callback_query()
+async def handle_callback(_, query):
+    if query.data == "home_":
+        await query.message.edit_text(
+            script.START_TXT.format(query.from_user.mention),
+            reply_markup=button
+        )
+    elif query.data == "tools_":
+        await query.message.edit_text(
+            script.TOOLS_TEXT,
+            reply_markup=buttons
+        )
+    elif query.data == "database_":
+        await query.message.edit_text(
+            script.DATABASE_TEXT,
+            reply_markup=buttons1
+        )
+    elif query.data == "force_":
+        await query.message.edit_text(
+            script.FORCE_TEXT,
+            reply_markup=buttons2
+        )
+    elif query.data == "shortener_":
+        await query.message.edit_text(
+            script.SHORTENER_TEXT,
+            reply_markup=buttons3
+        )
+    elif query.data == "maintainer_":
+        await query.answer(
+            "sᴏᴏɴ.... \nʙᴏᴛ ᴜɴᴅᴇʀ ɪɴ ᴍᴀɪɴᴛᴀɪɴᴀɴᴄᴇ",
+            show_alert=True
+        )
+    elif query.data == "close_data":
+        try:
+            await query.message.delete()
+            if query.message.reply_to_message:
+                await query.message.reply_to_message.delete()
+        except Exception as e:
+            print(f"Error deleting messages: {e}")
+
+
+                  
