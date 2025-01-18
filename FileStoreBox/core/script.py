26f40383485690cc3a4afba2b80a06379dddc35a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-19 02:12:13 +0530

    Update script.py

--- FileStoreBox/core/script.py
+++ FileStoreBox/core/script.py
@@ -1,4 +1,5 @@
 START_TEXT = "Hi {}! \n\nI’m File Store Box, the perfect place to store your files and videos. With unlimited storage, I’m not just a bot—I’m your next-level storage partner!"
+FORCE_SUB = ""
 
 TOOLS_TEXT = """
 **I am a File Store Box Bot with an unlimited database where users can store their media files securely.**
@@ -24,3 +25,9 @@ Use this feature if necessary for specific actions or settings.
 
 **This bot simplifies file management and ensures your media is always accessible through shareable links.**
 """
+
+
+DATABASE_TEXT = "**In a database tool, you can set a channel ID where all your media data will be stored, and from there, you'll be able to access your media.**"
+FORCE_TEXT = "**In a force sub-channel, you can make users join your channel forcibly, and only then will they be able to use your bot.**"
+SHORTENER_TEXT = "**In a shortener tool, you can add your shortener url and key, which the bot will use to provide users with short urls.**"
+
