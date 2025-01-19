d0602249a743b181230d4641ab0d1b2f9f6f5395
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-20 00:05:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN","7862204697:AAHxM0yBr3Xwnw9_HLbEBcActKQFk0cDySc")
 OWNER_ID = int(getenv("OWNER_ID", "6107581019"))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Extractor:ohUUMrBcLaCxlWZv@cluster0.nsjqx8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://FileStoreBox:file696store@cluster0.qrlbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
 HOST_URL = "https://t.me/"
 
