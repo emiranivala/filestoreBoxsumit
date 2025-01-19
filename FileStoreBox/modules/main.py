ddd2028500b698a5cc10567cf9d2080fc77880aa
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-19 19:19:13 +0530

    Update main.py

--- FileStoreBox/modules/main.py
+++ FileStoreBox/modules/main.py
@@ -55,3 +55,38 @@ async def watcher(_, message):
         
 
 
+@app.on_message(filters.command("batch") & filters.private)
+async def batch_(_, message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    data = await toolsdb.get_data(user_id)
+    
+    if data and data.get("channel_id"):
+        channel_id = data.get("channel_id")
+        
+        try:
+            start = await app.ask(chat_id, text="**Send me the link from where you want to start the batch.**", timeout=60)
+            start_id = start.text
+
+            last = await app.ask(chat_id, text="**Send me the link from where you want to end the batch.**", timeout=60)
+            last_id = last.text
+
+            encrypt_id = await main_func.base64_encrypt(f"{user_id}_{start_id}_{last_id}")
+            bot_link = f"https://telegram.dog/{BOT_USERNAME}?start=FileBox_{encrypt_id}"
+            
+            if data.get("api_url") and data.get("api_key"):
+                short_link = await main_func.short_link(user_id, bot_link)
+                buttons = [
+                    [InlineKeyboardButton("🔗 Bot Link", url=bot_link)],
+                    [InlineKeyboardButton("🏓 Shortener Link", url=short_link)],
+                ]
+            else:
+                buttons = [[InlineKeyboardButton("🔗 Bot Link", url=bot_link)]]
+
+            await message.reply_text(script.POST_TEXT, reply_markup=InlineKeyboardMarkup(buttons))
+        except asyncio.TimeoutError:
+            await message.reply_text("**You took too long to respond. Please try again.**")
+    else:
+        await message.reply_text("**Channel ID not found or not configured!**\n\n<i>First, add your database channel then you will be able to store anything in the bot.</i>")
+
+
