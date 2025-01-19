81422a1af8a6cf7c98ed83f48010edfe5b004841
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-19 22:05:18 +0530

    Update start.py

--- FileStoreBox/modules/start.py
+++ FileStoreBox/modules/start.py
@@ -140,11 +140,11 @@ async def handle_callback(_, query):
         else:
             print("unknown view query")
 		
-    elif query.data.startswith("checksup"):
+    elif query.data.startswith("checksub"):
 	task = query.data.split("#")[1]
         await more_func.fetch_files(_, query.message, encrypt_mode=False)
 	    
-    elif query.data.startswith("batchSup"):
+    elif query.data.startswith("batchSub"):
 	task = query.data.split("#")[1]
         await more_func.batch_files(_, query.message, encrypt_mode=False)
 	
