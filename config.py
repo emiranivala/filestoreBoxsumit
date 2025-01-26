38d0f0263293ddd9276209110551ac5fab28aa4a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-01-26 20:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ BOT_TOKEN = getenv("BOT_TOKEN","7862204697:AAHxM0yBr3Xwnw9_HLbEBcActKQFk0cDySc")
 OWNER_ID = int(getenv("OWNER_ID", "6107581019"))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://FileStoreBox:file696store@cluster0.qrlbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-HOST_URL = "https://t.me/"
+
 
